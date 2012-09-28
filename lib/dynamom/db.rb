84a2c508783f1c34cd52377546e08825e2a8e2ad
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-27 22:36:14 -0500

    more cleanup

--- lib/dynamom/db.rb
+++ lib/dynamom/db.rb
@@ -24,14 +24,20 @@ module DynaMom
     end
 
     def dynamo
-      @dynamo ||= AWS::DynamoDB.new(access_key_id: Config.aws_access_key_id, secret_access_key: Config.aws_secret_access_key)
+      @dynamo ||= AWS::DynamoDB.new(access_key_id: Config.aws_access_key_id, 
+                                    secret_access_key: Config.aws_secret_access_key)
     end
 
     def tables
       dynamo.tables.map do |t| 
         t.load_schema
       end.map do |t|
-        { :name => t.name, :read_capacity_units => t.read_capacity_units, :write_capacity_units => t.write_capacity_units, :status => t.status }
+        { 
+          :name => t.name, 
+          :read_capacity_units => t.read_capacity_units, 
+          :write_capacity_units => t.write_capacity_units, 
+          :status => t.status 
+        }
       end
     end
   end
